e60a4e2c1ea966534a602440ae63c6f6b3b017f2
Derek Dumouchel <ddumouch@bu.edu>
2022-10-27 16:23:49 -0400

    error

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,19 @@
+import sys
+import subprocess
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+
+import csv
+import pandas as pd
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    # with open(csv_file_path, 'r') as read_obj:
+    #     csv_reader = csv.reader(read_obj)
+    #     list_of_csv = list(csv_reader)
+    #     return list_of_csv
+    
+    df = pd.read_csv(csv_file_path, delimiter=',')
+    list_of_rows = [list(row) for row in df.values]
+    return list_of_rows
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,4 @@
+from math import sqrt
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +6,18 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    nominator = x.symmetric_difference(y)
+    denominator = x.union(y)
+    distance = len(nominator)/len(denominator)
+    
+    return distance
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
-# Feel free to add more
+    return dot(x,y) / ( (dot(x,x) **.5) * (dot(y,y) ** .5) )
+    # raise NotImplementedError()
+def dot(x,y): 
+    return (sum(a*b for a,b in zip(x,y)))
+    # Feel free to add more
